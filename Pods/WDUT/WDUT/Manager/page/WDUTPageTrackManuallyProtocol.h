46b4d8164ab5413ec3fe8719d592bc8ea18bfd09
feimatayan <2440848519@qq.com>
2021-10-20 23:45:42 +0800

    删除pods

--- Pods/WDUT/WDUT/Manager/page/WDUTPageTrackManuallyProtocol.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by shazhou on 2018/7/18.
-// Copyright (c) 2018 Weidian. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@protocol WDUTPageTrackManuallyProtocol <NSObject>
-@end
--- Pods/WDUtility/WDUtility/Sources/WDUtility.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by Henson on 9/28/15.
-// Copyright (c) 2015 Henson. All rights reserved.
-//
-
-#import "WDFoundation.h"
-#import "WDUIKit.h"
